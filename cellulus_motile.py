7553cb4730d44dc2a4cc0608a438ad6bc3854ad5
lmanan <manan.lalit@gmail.com>
2024-01-12 15:15:22 -0500

    1. Add code for saving tifs with correct tracked labels 2. Add SSVM code

--- cellulus_motile.py
+++ cellulus_motile.py
@@ -167,13 +167,159 @@ if __name__ == "__main__":
     print(f"Cand graph has {cand_graph.number_of_nodes()} nodes")
 
     solution, solver = solve_with_motile(cand_graph)
-
     solution_nx_graph = get_solution_nx_graph(solution, solver)
 
-    evaluate_with_traccuracy(ds_name, ctc_data_path, solution_nx_graph, segmentation)
+    # evaluate_with_traccuracy(ds_name, ctc_data_path, solution_nx_graph, segmentation)
 
+
+solver.weights
+
+# ## Resave segmentations with the tracking result
+
+# Uncomment below two cells
+
+
+def resave_tifs(solution_nx_graph, segmentation):
+    tracked_masks = np.zeros_like(segmentation)
+    new_mapping = {}
+    res_track = {}
+    id_counter = 1
+    for in_node, out_node in tqdm(solution_nx_graph.edges()):
+        t_in, id_in = in_node.split("_")
+        t_out, id_out = out_node.split("_")
+        t_in, id_in = int(t_in), int(id_in)
+        t_out, id_out = int(t_out), int(id_out)
+        num_out_edges = len(solution_nx_graph.out_edges(in_node))
+        if num_out_edges == 1:
+            if in_node in new_mapping.keys():
+                # i.e. continuation of an existing edge
+                res_track[new_mapping[in_node]][2] = t_out
+                tracked_masks[t_in][segmentation[t_in] == id_in] = new_mapping[in_node]
+                new_mapping[out_node] = new_mapping[in_node]
+                tracked_masks[t_out][segmentation[t_out] == id_out] = new_mapping[
+                    out_node
+                ]
+            else:
+                # i.e. start of a new edge
+                res_track[id_counter] = [0, t_in, t_out]
+                new_mapping[in_node] = id_counter
+                new_mapping[out_node] = id_counter
+                tracked_masks[t_in][segmentation[t_in] == id_in] = id_counter
+                tracked_masks[t_out][segmentation[t_out] == id_out] = id_counter
+                id_counter += 1
+        elif num_out_edges == 2:
+            out_edge1, out_edge2 = solution_nx_graph.out_edges(in_node)
+            _, out_node1 = out_edge1
+            _, out_node2 = out_edge2
+            t_out1, id_out1 = out_node1.split("_")
+            t_out1, id_out1 = int(t_out1), int(id_out1)
+            t_out2, id_out2 = out_node2.split("_")
+            t_out2, id_out2 = int(t_out2), int(id_out2)
+            if in_node in new_mapping.keys():
+                # i.e. in node was connected by one outgoing edge previously
+                res_track[new_mapping[in_node]][2] = t_in
+                tracked_masks[t_in][segmentation[t_in] == id_in] = new_mapping[in_node]
+                if out_node1 not in new_mapping:
+                    new_mapping[out_node1] = id_counter
+                    tracked_masks[t_out1][segmentation[t_out1] == id_out1] = id_counter
+                    res_track[id_counter] = [new_mapping[in_node], t_out1, t_out1]
+                    id_counter += 1
+                if out_node2 not in new_mapping:
+                    new_mapping[out_node2] = id_counter
+                    tracked_masks[t_out2][segmentation[t_out2] == id_out2] = id_counter
+                    res_track[id_counter] = [new_mapping[in_node], t_out2, t_out2]
+                    id_counter += 1
+            else:
+                # i.e.
+                res_track[id_counter] = [
+                    0,
+                    t_in,
+                    t_in,
+                ]  # since it divides immediately after
+                new_mapping[in_node] = id_counter
+                tracked_masks[t_in][segmentation[t_in] == id_in] = id_counter
+                id_counter += 1
+                if out_node1 not in new_mapping:
+                    new_mapping[out_node1] = id_counter
+                    tracked_masks[t_out1][segmentation[t_out1] == id_out1] = id_counter
+                    res_track[id_counter] = [new_mapping[in_node], t_out1, t_out1]
+                    id_counter += 1
+                if out_node2 not in new_mapping:
+                    new_mapping[out_node2] = id_counter
+                    tracked_masks[t_out2][segmentation[t_out2] == id_out2] = id_counter
+                    res_track[id_counter] = [new_mapping[in_node], t_out2, t_out2]
+                    id_counter += 1
+    tifffile.imwrite("/Users/lalitm/Desktop/new_segmentations.tif", tracked_masks)
+    return new_mapping, res_track, tracked_masks
+
+
+new_mapping, res_track, new_segmentations = resave_tifs(solution_nx_graph, segmentation)
+res_track_list = []
+for key in res_track.keys():
+    res_track_list.append(
+        [key, res_track[key][1], res_track[key][2], res_track[key][0]]
+    )
+np.savetxt("/Users/lalitm/Desktop/res_track.txt", np.asarray(res_track_list), fmt="%i")
 
 # # Optional stuff
 # - learn weights with ssvm (with small portion of GT)
 # - add fake node score (random from .5 to 1, or something)
 # - add jitter to node locations/dropout/add extra nodes
+
+# ## SSVM
+
+# ### read man_track.txt
+
+gt_tracks = np.loadtxt(ctc_data_path / "man_track.txt")
+print(gt_tracks)
+
+# ## let's set the id `4` to be ground truth (i.e. third row)
+
+# For this, we would have to find predicted segmentations which 
+# overlap ground truth id 4 between time points 40 and 86
+
+
+gt_mask_names = list((ctc_data_path).glob("*.tif"))
+gt_id = 4
+start_t = 40
+end_t = 86
+for t in tqdm(range(start_t, end_t)):
+    ma_gt_t = tifffile.imread(gt_mask_names[t])
+    ma_gt_tp1 = tifffile.imread(gt_mask_names[t + 1])
+    y_t, x_t = np.where(ma_gt_t == gt_id)
+    y_tp1, x_tp1 = np.where(ma_gt_tp1 == gt_id)
+    ids_t = np.unique(segmentation[t][y_t, x_t])
+    ids_t = ids_t[ids_t != 0]
+    ids_tp1 = np.unique(segmentation[t + 1][y_tp1, x_tp1])
+    ids_tp1 = ids_tp1[ids_tp1 != 0]
+    if len(ids_t) == 1 and len(ids_tp1) == 1:
+        cand_graph.nodes[str(t) + "_" + str(ids_t[0])]["gt"] = True
+        cand_graph.nodes[str(t + 1) + "_" + str(ids_tp1[0])]["gt"] = True
+        cand_graph.edges[
+            (str(t) + "_" + str(ids_t[0]), str(t + 1) + "_" + str(ids_tp1[0]))
+        ]["gt"] = True
+
+print(f"Cand graph has {cand_graph.number_of_nodes()} nodes")
+
+
+def fit_weights(solver, max_iterations=5):
+    start_time = time.time()
+    solver.fit_weights(
+        gt_attribute="gt", regularizer_weight=0.01, max_iterations=max_iterations
+    )
+    optimal_weights = solver.weights
+    print(f"Optimal weights are {optimal_weights}")
+    solution = solver.solve()
+    print(f"Solution took {time.time() - start_time} seconds")
+    return solution, solver
+
+
+# motile_cand_graph = add_gt_track(motile_cand_graph, gt_track_graph)
+solution, solver = fit_weights(solver)
+solution_nx_graph = get_solution_nx_graph(solution, solver)
+
+solver.weights
+
+# +
+# solution_nx_graph.nodes()
+# -
